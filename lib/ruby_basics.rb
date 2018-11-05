a3e05911fd1308b594cb094ea1d1bb356f5ac2c1
Nathan Bell <nathanbell09@gmail.com>
2018-11-05 19:27:03 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -7,15 +7,15 @@ def assign_variable(value)
 end
 
 def argue(arg)
-  arg  
+  arg
 end
 
-def greeting
-
+def greeting(greeting, name)
+  
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
